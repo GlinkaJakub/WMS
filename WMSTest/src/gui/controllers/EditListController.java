01a05cd7ef75b19a8a2d56b0811c18d09ff3734e
Jakub Glinka <37155311+GlinkaJakub@users.noreply.github.com>
2018-05-16 16:22:51 +0200

    update z editem danych

---
+++ WMSTest/src/gui/controllers/EditListController.java
@@ -0,0 +1,27 @@
+package gui.controllers;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class EditListController {
+
+    @FXML
+    private TextField idTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField weightTextField;
+
+    @FXML
+    private TextField heightTextField;
+
+    @FXML
+    private TextField lenghtTextField;
+
+
+
+
+}
--- WMSTest/src/gui/controllers/EditWindowController.java
+++ WMSTest/src/gui/controllers/EditWindowController.java
@@ -5,15 +5,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class EditWindowController {
-
 
 
     ObservableList list = FXCollections.observableArrayList();
@@ -21,10 +23,51 @@ public class EditWindowController {
 
     @FXML
     private ListView<String> editListView;
+    static class XCell extends ListCell<String> {
 
+        Button button = new Button("Del");
+        Button button2 = new Button("Edit");
+        HBox hbox = new HBox();
+        Pane pane = new Pane();
+        Label label = new Label("");
 
-    @FXML
-    private Button editButton;
+        public XCell( ) {
+            super();
+
+            hbox.getChildren().addAll(label, pane, button2, button);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+            button2.setOnMouseClicked(e -> {
+                System.out.println("Tutaj wjebac okno");
+                FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/gui/fxml/EditListWindow.fxml"));
+                AnchorPane anchorPane = null;
+                try {
+                    anchorPane = loader.load();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                Scene scene = new Scene(anchorPane);
+                Stage currentStage = new Stage();
+                currentStage.setScene(scene);
+                currentStage.setTitle("WMS");
+                currentStage.show();
+
+            });
+            button.setOnAction(event -> getListView().getItems().remove(getItem()));
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            setText(null);
+            setGraphic(null);
+
+            if (item != null && !empty) {
+                label.setText(item);
+                setGraphic(hbox);
+            }
+        }
+    }
+
 
     private MainController mainController;
 
@@ -70,6 +113,9 @@ public class EditWindowController {
 
         editListView.setCellFactory(TextFieldListCell.forListView());
         editListView.setEditable(true);
+        editListView.setCellFactory(param -> new XCell());
+
+//        editListView.setCellFactory(param -> new XCell(button2));
 //        editListView.getEditingIndex();
     }
 
@@ -85,13 +131,9 @@ public class EditWindowController {
 
     }
 
+
     public void addObject(ActionEvent actionEvent) {
                 editListView.getItems().add(0, "new");
     }
 
-    public void deleteObject(ActionEvent actionEvent) {
-       int  i = editListView.getEditingIndex();
-        editListView.getItems().remove(i);
-        i--;
-    }
 }
